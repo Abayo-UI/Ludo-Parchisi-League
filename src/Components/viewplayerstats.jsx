0c80936e4f4513f52f86c48f42999722c292e40b
unknown <leslie2000abayo@gmail.com>
2025-01-17 09:42:56 -0800

    fixed viewplayer stats component

--- src/Components/viewplayerstats.jsx
+++ src/Components/viewplayerstats.jsx
@@ -8,13 +8,13 @@ export default function ViewPlayerStats({stats}){
               { playersProfile.map( player => 
               stats === player.photo &&
               <div class="row height">
-              <img alt="" src={`/images/${player.photo}`} class="col-lg-3 col-5 height ms-3"/>
-              <div class="col-lg-5 col-6 text-light">
-              <p>{player.name}</p>
-              <p>Rivals: {player.rivals}</p>
-              <p>Pos in last year: {player["last league position"]}/6</p>
-              <p>Points deducted: {player["points deducted due to irregularities"]}</p>
-              <p>Unplayed games: </p>
+              <img alt="" src={`/images/${player.photo}`} class="col-lg-3 col-4 height ms-3"/>
+              <div class="col-lg-5 col-7 text-light height">
+              <p><strong class="text-dark text-decoration-underline">{player.name.toUpperCase()}</strong></p>
+              <p class="profile-details"><strong class="text-dark">Rivals:</strong>{player.rivals.map( rival => rival + " ")}</p>
+              <p class="profile-details"><strong class="text-dark">Pos last year:</strong> {player["last league position"]}/6</p>
+              <p class="profile-details"><strong class="text-dark">Points deducted:</strong> {player["points deducted due to irregularities"]}</p>
+              <p class="profile-details"><strong class="text-dark">Unplayed games: </strong></p>
               </div>
               </div>
               )
