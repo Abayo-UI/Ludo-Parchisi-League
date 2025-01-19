64c1809eb50a43b6f14771149221e78a5cb83ce4
unknown <leslie2000abayo@gmail.com>
2025-01-19 09:51:16 -0800

    viewplayer component height

--- src/Components/viewplayerstats.jsx
+++ src/Components/viewplayerstats.jsx
@@ -9,8 +9,8 @@ export default function ViewPlayerStats({stats}){
               stats === player.photo &&
               <div class="row height">
               
-              <img alt="" src={`/images/${player.photo}`} class="col-lg-3 col-4 height ms-3"/>
-              <div class="col-lg-5 col-7 text-light height">
+              <img alt="" src={`/images/${player.photo}`} class="col-lg-3 col-5  ms-3"/>
+              <div class="col-lg-5 col-7 text-light ms-3">
               <p><strong class="text-dark text-decoration-underline">{player.name.toUpperCase()}</strong></p>
               <p class="profile-details"><strong class="text-dark">Rivals:</strong>{player.rivals.map( rival => rival + " ")}</p>
               <p class="profile-details"><strong class="text-dark">Pos last year:</strong> {player["last league position"]}/6</p>
