afbbe7817c0c537816b69403c44fcbd775f2b3a0
unknown <leslie2000abayo@gmail.com>
2025-01-28 11:15:28 -0800

    Matchday 5 Results

--- src/Components/playersMatchData.js
+++ src/Components/playersMatchData.js
@@ -2,7 +2,7 @@ import data from "./data";
 
 export const  enterPlayersPositions = {
     "Donald":  [ 3,3,2,3,4,3,1,2,2,3],
-    "Kamar":   [ 1,3,4,1,2,3,4,1,2],
+    "Kamar":   [ 1,3,4,1,2,3,2,4,1,2],
     "Mokaya":  [ 2,4,1,3,3,1],
     "Leslie":  [ 1,4,1,1,2,3,2,1],
     "Rony":    [ 2,2,1,4,2,3,1,3,3],
